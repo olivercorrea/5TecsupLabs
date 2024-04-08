842123008eebfb58cf36a382195cc4bab609eb32
olivercorrea <olivercorreacabana@outlook.es>
2024-04-08 12:45:32 -0500

    refactor(App.js): agregar import de TouchableOpacity y Alert, crear función openImagePickerAsync, actualizar return en App function feat(App.js): agregar botón para abrir el picker de imágenes y mostrar alerta de permisos no concedidos

--- AplMovMult/S5/proyecto05_06/App.js
+++ AplMovMult/S5/proyecto05_06/App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import React from 'react'
+import * as ImagePicker from 'expo-image-picker'
 
-export default function App() {
+const App = () => {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert('Se requiere permiso para acceder al equipo');
+      return;
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hola Mundo!!</Text>
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style = {styles.button}
+      >
+        <Text style={styles.buttonText}>Presiona aquí</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  button: {
+    backgroundColor: 'blue',
+    padding: 7,
+    marginTop: 10
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20
+  }
 });
+
+export default App

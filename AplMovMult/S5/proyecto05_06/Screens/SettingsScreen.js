8abdf5bdd4d08af6e66d0126e03fa2006cf80d73
olivercorrea <olivercorreacabana@outlook.es>
2024-04-14 11:10:37 -0500

    crear pantalla de configuración SettingsScreen.js
    feat(Screens/SettingsScreen.js): crear nueva pantalla de configuraciones con Text y View componentes.

---
+++ AplMovMult/S5/proyecto05_06/Screens/SettingsScreen.js
@@ -0,0 +1,17 @@
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const SettingsScreen = () => {
+    return ( 
+        <View>
+            <Text 
+            style = {{
+                fontSize: 30,
+                textAlign: 'center',
+                marginTop: '20%'
+            }}>Settings Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;

8e9ad280c8b46850ae4d99b2bcb9b64434bbb033
olivercorrea <olivercorreacabana@outlook.es>
2024-04-14 11:14:04 -0500

    crear pantalla StackScreen con texto "Stack Screen"
    crear archivo StackScreen.js y exportar una función de renderizado para la pantalla.

---
+++ AplMovMult/S5/proyecto05_06/Screens/StackScreen.js
@@ -0,0 +1,17 @@
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const StackScreen = () => {
+    return ( 
+        <View>
+            <Text 
+            style = {{
+                fontSize: 30,
+                textAlign: 'center',
+                marginTop: '20%'
+            }}>Stack Screen</Text>
+        </View>
+    )
+}
+
+export default StackScreen;

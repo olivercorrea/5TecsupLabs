83494582614f4bc6431a1a1339fb282cda084aa3
olivercorrea <olivercorreacabana@outlook.es>
2024-04-14 11:08:27 -0500

    crear pantalla principal "HomeScreen.js" con texto "Home Screen"
    En este commit se ha creado un nuevo archivo `HomeScreen.js` que contiene una vista de React Native con un texto centrado y un tamaño de fuente de 30 píxeles, mostrando el texto "Home Screen".

---
+++ AplMovMult/S5/proyecto05_06/Screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+    return ( 
+        <View>
+            <Text style = {{
+                fontSize: 30,
+                textAlign: 'center',
+                marginTop: '20%'
+            }}>Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;

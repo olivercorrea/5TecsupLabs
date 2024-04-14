8eb27d33d49e430a89b522a7dff4dfcd9d8c1628
olivercorrea <olivercorreacabana@outlook.es>
2024-04-14 11:43:40 -0500

    refactor(Navigation.js): agregar MaterialCommunityIcons y configurar opciones de tabas feat(Navigation.js): mejorar el diseño de la navegación con iconos personalizados

--- AplMovMult/S5/proyecto05_06/Navigation.js
+++ AplMovMult/S5/proyecto05_06/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import HomeScreen from './Screens/HomeScreen';
 import SettingsScreen from './Screens/SettingsScreen';
@@ -10,9 +11,42 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
     return (
-        <Tab.Navigator        >
-            <Tab.Screen name='Home' component={HomeScreen}/>
-            <Tab.Screen name='Settings' component={SettingsScreen}/>
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                tabBarActiveTintColor: 'purple',
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={MyStack}
+                options={{
+                    tabBarLabel: 'Inicio',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="home"
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                    tabBarBadge: 3,
+                    headerShown: false
+                }}
+            />
+            <Tab.Screen
+                name="Settings"
+                component={SettingsScreen}
+                options={{
+                    tabBarLabel: 'Ajustes',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="brightness-5"
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     )
 }

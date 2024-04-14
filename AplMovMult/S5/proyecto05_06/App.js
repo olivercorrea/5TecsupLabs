5148534be034260c4b551858a630a0689d271b92
olivercorrea <olivercorreacabana@outlook.es>
2024-04-14 11:32:23 -0500

    refactor(App.js): 1. Eliminar importaciones innecesarias y reducir el tamaño del archivo. 2. Crear un nuevo archivo Navigation.js para exportar la configuración de navegación. 3. Cambiar App a una función de renderizado puro que retorna <Navigation/>. 4. Eliminar el estado local de selectedImage y la función openImagePickerAsync. 5. Importar React en la línea 1 en lugar de importar todo a partir de 'react-native'. 6. Cambiar el nombre de App a una constante para que sea una función de fabricación. 7. Eliminar estilos innecesarios y agregarlos a un archivo separado. 8. Usar export default en lugar de export default function para simplificar la exportación del componente.
    reason: Mejora el código eliminando importaciones innecesarias, reduciendo el tamaño del archivo y mejorando la estructura del proyecto.

--- AplMovMult/S5/proyecto05_06/App.js
+++ AplMovMult/S5/proyecto05_06/App.js
@@ -1,69 +1,10 @@
-import { View, Text, StyleSheet, TouchableOpacity, Alert, Image } from 'react-native'
-import React, {useState} from 'react'
-import * as ImagePicker from 'expo-image-picker'
+import React from 'react';
+import Navigation  from './Navigation';
 
-const App = () => {
+export default function App() {
+    return (
+        <Navigation/>
+    )
+}
 
-  const [selectedImage, setSelectedImage] = useState(null)
-
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
-
-    if (permissionResult.granted === false) {
-      alert('Se requiere permiso para acceder al equipo');
-      return;
-    }
-
-    const pickerResult = await ImagePicker.launchImageLibraryAsync()
-
-    if (pickerResult.canceled === true) {
-      return;
-    }
-
-    setSelectedImage({localUri: pickerResult.uri})
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text>Hola Mundo!!</Text>
-      <Image
-        source={{uri: selectedImage !== null
-          ? selectedImage.localUri
-          : "https://picsum.photos/200/200"
-        }}
-        style={styles.image}
-      />
-      <TouchableOpacity
-        onPress={openImagePickerAsync}
-        style = {styles.button}
-      >
-        <Text style={styles.buttonText}>Presiona aquí</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  button: {
-    backgroundColor: 'blue',
-    padding: 7,
-    marginTop: 10
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 20
-  },
-  image: {
-    height: 200,
-    width: 200,
-    borderRadius: 100,
-    resizeMode: 'contain'
-  }
-});
-
-export default App;
+// Path: Navigation.js
